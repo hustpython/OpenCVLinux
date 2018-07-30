17d3d50013d69bc62f81673d0b339d443f29b7b4
mxq <1529806383@qq.com>
2018-07-30 21:51:57 +0800

    add orcdigits py and cpp

---
+++ CVORCDigital/README.md
@@ -0,0 +1,6 @@
+## 标记数据
+![](docimgs/label.png)
+## 测试图片
+![](docimgs/test.png)
+## 测试结果
+![](docimgs/recongize.png)
---
+++ CVORCDigital/src/labeldata.cpp
@@ -0,0 +1,50 @@
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main(int argc,char ** argv)
+{
+    Mat thr,gray,con;
+    Mat src = imread(argv[1],1);
+    cvtColor(src,gray,CV_BGR2GRAY);
+    threshold(gray,thr,200,255,THRESH_BINARY_INV);
+    thr.copyTo(con);
+    vector < vector <Point> > contours;
+    vector<Vec4i>hierarchy;
+    Mat sample;
+    Mat response_array;
+    findContours(con,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE);
+    for(int i = 0;i<contours.size();i=hierarchy[i][0])
+    {
+        Rect r = boundingRect(contours[i]);
+        rectangle(src,Point(r.x,r.y),Point(r.x+r.width,r.y+r.height),Scalar(0,0,255),2,8,0);
+        Mat ROI = thr(r);
+        Mat tmp1,tmp2;
+        resize(ROI,tmp1,Size(10,10),0,0,INTER_LINEAR);
+        tmp1.convertTo(tmp2,CV_32FC1);
+        sample.push_back(tmp2.reshape(1,1));
+        imshow("src",src);
+        int c = waitKey(0);
+        c -= 0x30;
+        if(c == 27)
+        {
+            return 1;
+        }
+        response_array.push_back(c);
+        rectangle(src,Point(r.x,r.y), Point(r.x+r.width,r.y+r.height), Scalar(0,255,0),2,8,0);
+    }
+    Mat response,tmp;
+    tmp = response_array.reshape(1,1);
+    tmp.convertTo(response,CV_32FC1);
+
+    FileStorage Data("TrainingData.yml",FileStorage::WRITE);
+    Data << "data" << sample;
+    Data.release();
+
+    FileStorage Label("LabelData.yml",FileStorage::WRITE);
+    Label << "label" << response;
+    Label.release();
+
+    imshow("src",src);
+    waitKey();
+}
