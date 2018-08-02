6af8b2552ba7fb5d314f2621a7104882091af050
mxq <1529806383@qq.com>
2018-08-02 19:10:21 +0800

    reconstruct pycode

--- HandinSVM/src/python/testsvm.py
+++ HandinSVM/src/python/testsvm.py
@@ -1,7 +1,8 @@
 # coding=utf-8
+from collections import Counter
+import ConfigParser
 from svm import *
 import numpy as np
-from collections import Counter
 import csv
 # global var
 # ========================
@@ -28,52 +29,108 @@ labellist = [[1,2],[2,3],[3,1]]
 #        ]
 #　如果为gauss核的话  ['Gauss',标准差]
 
-# 获取预测结果 1/-1
-def showres(data,prelist):
-    svm=SVM(data,'Line',1000,0.02,0.001)
-    svm.train()
-    #print("train done")
-    res = svm.predict(prelist)
-    # print(svm.a)
-    # print(svm.w)
-    # print(svm.b)
-    return res[0]
+# ==========将训练得到的参数写入配置文件中========
+class Test():
+    def __init__(self):
+        self.a = []
+        self.b = []
+        self.conf = ConfigParser.ConfigParser()
+        if "param1" not in self.conf.sections():
+           self.conf.add_section("param1")
+        if "param2" not in self.conf.sections():
+           self.conf.add_section("param2")
+        if "param3" not in self.conf.sections():
+           self.conf.add_section("param3")
+
+    def readconfig(self):
+
+        self.conf.read('alphaconfig.ini')
+        res1a = self.conf.get("param1", "alpha")
+        res1b = self.conf.get("param1", "b")
+
+        res2a = self.conf.get("param2", "alpha")
+        res2b = self.conf.get("param2", "b")
+
+        res3a = self.conf.get("param3", "alpha")
+        res3b = self.conf.get("param3", "b")
+
+        return[[res1a,res1b],[res2a,res2b],[res3a,res3b]]
+
+    def writeconf(self,i): 
+        self.conf.read('alphaconfig.ini')
+        self.conf.set("param"+str(i), "alpha", self.a)
+        self.conf.set("param"+str(i),"b",self.b)
+        self.conf.write(open('alphaconfig.ini', 'w'))
+    def train(self,data,testdata,testlabel):
+            for k ,traindata in enumerate(data):
+                print("正在训练数据！")
+                error_rate = 1.1
+                for i in range(10):
+                    svmtrain=SVM(traindata,'Line',1000,0.02,0.001)
+                    svmtrain.train()
+                    self.a = svmtrain.a 
+                    self.b = svmtrain.b
+                    test_error_rate = self.get_error(testdata[k],testlabel[k],[traindata],k,train=True)
+                    if  test_error_rate < error_rate:
+                        error_rate = test_error_rate 
+                        self.writeconf(k+1)
+                print(error_rate)
+
+    def showres(self,data,prelist):
+        svmtest=SVM(data,'Line',1000,0.02,0.001)
+        svmtest.a = self.a
+        svmtest.b = self.b
+        res = svmtest.predict(prelist)
+        return res[0]
 
 # 找到多分类中频率最高的类标
-def findmaxfre(reslist):
-    return Counter(reslist).most_common(1)[0][0]
+    def findmaxfre(self,reslist):
+        return Counter(reslist).most_common(1)[0][0]
 
 # 验证错误率
-def get_error(data,label):
-    count = 0
-    print("正在验证错误率!")
-    for i,predictdata in enumerate(data):
-        res = predict(predictdata)       
-        if res != label[i]:
-            count += 1
-        print(predictdata,i+1,res,label[i])
-    print("count:",str(count))
-    print("error_rate:",str(float(float(count)/float(len(data)))))
+    def get_error(self,data,label,svmdata,dataindex,train=False):
+        count = 0
+        for i,predictdata in enumerate(data):
+            res = self.predict(predictdata,svmdata,dataindex,train)       
+            if res != label[i]:
+                count += 1
+            #print(i+1,res,label[i])
+        error_rate = float(float(count)/float(len(data)))
+        #print("count:",str(count))
+        #print("error_rate:",str(error_rate))
+        return error_rate
 
 #　返回预测结果
-def predict(predictlist):
-    reslabel = []
-    for j,data in enumerate([data12,data23,data31]):
-        res = showres(data,predictlist)
-        labelindex = [1,-1].index(res)
-        reslabel.append(labellist[j][labelindex])
-    return findmaxfre(reslabel)
+    def predict(self,predictlist,svmdata,dataindex,train):
+        reslabel = []
+        for j,data in enumerate(svmdata):
+            if not train:
+                self.a = [float(f) for f in eval(self.readconfig()[j][0])]
+                self.b = float(self.readconfig()[j][1])
+            res = self.showres(data,predictlist)
+            labelindex = [1,-1].index(res)
+            if dataindex <=0:
+               reslabel.append(labellist[j][labelindex])
+            else:
+               reslabel.append(labellist[dataindex][labelindex])
+        return self.findmaxfre(reslabel)
 
 # 单独预测某一组数据
-def test(predictlist):
-    print("正在预测！")
-    res = predict(predictlist)
-    print(str(res)+"类")
+    def test(self,predictlist,svmdata,dataindex):
+        print("正在预测！")
+        res = self.predict(predictlist,svmdata,dataindex,False)
+        print(str(res)+"类")
 
 # 主函数　原来验证错误率和预测数据
 if __name__ == '__main__':
     testdata = [[float("%.2f"%f) for f in sample[1:]] for sample in sn_data]
     testdatalabel = [int(sample[0]) for sample in sn_data]
-    get_error(testdata,testdatalabel)
-    prelist = [14.13, 4.1, 2.74, 24.5, 96.0, 2.05, 0.76, 0.56, 1.35, 9.2, 0.61, 1.6, 560.0] # 1类
-    #test(prelist)
+    testclass = Test()
+    traindata = [data12,data23,data31]
+    testdatalist = [testdata[:sn1+sn2],testdata[sn1:],testdata[:sn1]+testdata[sn1+sn2:]]
+    testdatalabellist = [testdatalabel[:sn1+sn2],testdatalabel[sn1:],testdatalabel[:sn1]+testdatalabel[sn1+sn2:]]
+    #testclass.train(traindata,testdatalist,testdatalabellist)
+    threeclass_rate = testclass.get_error(testdata,testdatalabel,traindata,-1)
+    print(1-threeclass_rate)
+    #prelist = [14.13, 4.1, 2.74, 24.5, 96.0, 2.05, 0.76, 0.56, 1.35, 9.2, 0.61, 1.6, 560.0] # 1类
+    #testclass.test(prelist,traindata,-1)
